22d997e2a0e0c2adf132785b4c221ae0f9898cc6
Luizefz <luizefsilva2@gmail.com>
2023-04-22 14:40:05 -0300

    fix: troca de cores dos componentes

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import Logo from "images/Fear-bro.svg"
+import Logo from "images/Cine Tag.png"
 import styles from "./Cabecalho.module.css"
 import CabecalhoLink from "components/CabecalhoLink";
 
