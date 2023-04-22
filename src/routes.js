1e082774a5d08d8088e19cb9a8c578f8ce45190d
Luizefz <luizefsilva2@gmail.com>
2023-04-22 14:52:28 -0300

    feat: add versel analytics

--- src/routes.js
+++ src/routes.js
@@ -5,20 +5,24 @@ import Favoritos from 'pages/Favoritos';
 import Player from 'pages/Player';
 import Erro404 from 'pages/Erro404';
 import PaginaBase from 'pages/Paginabase';
+import { Analytics } from '@vercel/analytics/react';
 
 
 function AppRoutes() {
     return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<PaginaBase />}>
-                    <Route index element={<Inicio />} />
-                    <Route path="favoritos" element={<Favoritos />}></Route>
-                    <Route path=':id' element={<Player />}></Route>
-                    <Route path='*' element={<Erro404 />}></Route>
-                </Route>
-            </Routes>
-        </Router >
+        <>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<PaginaBase />}>
+                        <Route index element={<Inicio />} />
+                        <Route path="favoritos" element={<Favoritos />}></Route>
+                        <Route path=':id' element={<Player />}></Route>
+                        <Route path='*' element={<Erro404 />}></Route>
+                    </Route>
+                </Routes>
+            </Router >
+            <Analytics />
+        </>
     )
 }
 
